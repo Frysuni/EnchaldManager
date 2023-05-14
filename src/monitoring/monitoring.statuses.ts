6f3ea725c0295d73a3222593d22af30285eb568c
Frysuni <ibragimzinatullin@mail.ru>
2023-05-14 16:56:24 +0500

    14,05

--- src/monitoring/monitoring.statuses.ts
+++ src/monitoring/monitoring.statuses.ts
@@ -1,6 +1,6 @@
 import { IA, InjectDiscordClient, On, Once } from "@discord-nestjs/core";
-import { ActivityType, Client, ColorResolvable, EmbedBuilder, Events, GuildEmoji, Interaction, PresenceData } from "discord.js"
-import { divideArray } from "./utils";
+import { ActivityType, Client, Colors, EmbedBuilder, Events, GuildEmoji, PresenceData } from "discord.js";
+import { parseAndDivideAndLimitPlayers } from "./utils";
 
 interface StatusInterface {
   presence: PresenceData
@@ -11,24 +11,24 @@ export class MonitoringStatuses {
   constructor(
     @InjectDiscordClient() private readonly client: Client,
   ) {}
-  
-  public getBackuping(color: ColorResolvable, serverName: string): StatusInterface {
+
+  public getBackuping(serverName: string): StatusInterface {
     return {
-      embed: this.getEmbedBase(color, serverName).setFields({ name: `Сервер находится на резервном копировании.`, value: '\u200B' }),
-      presence: { status: 'dnd', activities: [{ type: ActivityType.Playing, name: 'BACKUPING'}] }
-    }
+      embed: this.getEmbedBase(Colors.Red, serverName).setFields({ name: `Сервер находится на резервном копировании.`, value: '\u200B' }),
+      presence: { status: 'dnd', activities: [{ type: ActivityType.Playing, name: 'BACKUPING' }] },
+    };
   }
 
-  public getRestarting(color: ColorResolvable, serverName: string): StatusInterface {
+  public getRestarting(serverName: string): StatusInterface {
     return {
-      embed: this.getEmbedBase(color, serverName).setFields({ name: `Сервер перезагружается... `, value: '\u200B' }),
-      presence: { status: 'idle', activities: [] }
-    }
+      embed: this.getEmbedBase(Colors.Yellow, serverName).setFields({ name: `Сервер перезагружается... `, value: '\u200B' }),
+      presence: { status: 'idle', activities: [] },
+    };
   }
 
-  public getServerStopped(color: ColorResolvable, serverName: string, lastOnline: number): StatusInterface {
+  public getServerStopped(serverName: string, lastOnline: number): StatusInterface {
     const presence: PresenceData = { activities: [], status: 'invisible' };
-    const embed = this.getEmbedBase(color, serverName);
+    const embed = this.getEmbedBase(Colors.Grey, serverName);
     embed.setFields({
       name: `Сервер выключен ${this.emojis.offline?.toString() ?? '{NO_EMOJI=:offline:}'}`,
       value: lastOnline ? `Был выключен <t:${lastOnline}:R>` : 'Ещё ни разу не был включен',
@@ -37,17 +37,16 @@ export class MonitoringStatuses {
     return {
       embed,
       presence,
-    }
+    };
   }
 
-  public getServerStarted(color: ColorResolvable, serverName: string, online: string, rawPlayers: string[] | undefined, restartAtInSeconds: number): StatusInterface {
+  public getServerStarted(serverName: string, online: string, rawPlayers: string[] | undefined, restartAtInSeconds: number): StatusInterface {
     const noOneIsOnline = online.startsWith('0');
     const title = noOneIsOnline ? 'Никого нет онлайн' : `Онлайн: ${online} игроков.`;
-    const parsePlayerName = (name: string): string => `• ${name.replace(/_/g, '\\_').replace(/\*/g, '\\*').replace(/~/g, '\\~').replace(/`/g, '\\`')}\n`;
 
-    const [firstPlayersHalf, secondPlayersHalf] = divideArray(rawPlayers?.map(parsePlayerName));
-    
-    const embed = this.getEmbedBase(color, serverName);
+    const [firstPlayersThird, secondPlayersThird, thirdPlayersThird] = parseAndDivideAndLimitPlayers(rawPlayers);
+
+    const embed = this.getEmbedBase(Colors.Green, serverName);
 
     embed.setFields({
       name: `Сервер работает ${this.emojis.online?.toString() ?? '{NO_EMOJI=:online:}'}`,
@@ -56,18 +55,26 @@ export class MonitoringStatuses {
 
     embed.addFields({
       name: title,
-      value: firstPlayersHalf.length ? firstPlayersHalf.join('') : (noOneIsOnline ? 'Присоединяйся скорее ;)' : 'Информация о списке игроков недоступна.'),
+      value: firstPlayersThird.length ? firstPlayersThird.join('') : (noOneIsOnline ? 'Присоединяйся скорее ;)' : 'Информация о списке игроков недоступна.'),
       inline: true,
     });
 
-    if (secondPlayersHalf.length) {
+    if (secondPlayersThird.length) {
       embed.addFields({
         name: '\u200B',
-        value: secondPlayersHalf.join(''),
+        value: secondPlayersThird.join(''),
         inline: true,
-      })
+      });
     }
-    
+
+    if (thirdPlayersThird.length) {
+      embed.addFields({
+        name: '\u200B',
+        value: thirdPlayersThird.join(''),
+        inline: true,
+      });
+    }
+
     const presence: PresenceData = {
       activities: [{
         type: ActivityType.Watching,
@@ -79,13 +86,13 @@ export class MonitoringStatuses {
     return {
       embed,
       presence,
-    }
+    };
   }
 
-  private getEmbedBase(color: ColorResolvable, serverName: string): EmbedBuilder {
+  private getEmbedBase(color: typeof Colors[keyof typeof Colors], serverName: string): EmbedBuilder {
     return new EmbedBuilder()
       .setColor(color)
-      .setTitle(`Мониторинг Enchald ${serverName}`)
+      .setTitle(`Мониторинг Enchald ${serverName}`);
   }
 
   private emojis: {
@@ -95,24 +102,24 @@ export class MonitoringStatuses {
   } = {
     online:  '{NO_EMOJI=:online:}',
     offline: '{NO_EMOJI=:offline:}',
-    reboot:  '{NO_EMOJI=:reboot:}'
+    reboot:  '{NO_EMOJI=:reboot:}',
   };
 
   @Once(Events.ClientReady)
   private setEmojis(): any {
     this.emojis = {
-      online:  this.client.emojis.cache.find(emoji => emoji.name?.toLowerCase() == 'online' )?.toString() ?? this.emojis.online,
+      online:  this.client.emojis.cache.find(emoji => emoji.name?.toLowerCase() == 'online')?.toString() ?? this.emojis.online,
       offline: this.client.emojis.cache.find(emoji => emoji.name?.toLowerCase() == 'offline')?.toString() ?? this.emojis.offline,
-      reboot:  this.client.emojis.cache.find(emoji => emoji.name?.toLowerCase() == 'reboot' )?.toString() ?? this.emojis.reboot,
-    }
+      reboot:  this.client.emojis.cache.find(emoji => emoji.name?.toLowerCase() == 'reboot')?.toString() ?? this.emojis.reboot,
+    };
   }
 
-  @On(Events.GuildEmojiCreate) private onGuildEmojiCreate (@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'create'); }
-  @On(Events.GuildEmojiDelete) private onGuildEmojiDelete (@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'delete'); }
-  @On(Events.GuildEmojiUpdate) private onGuildEmojiUpdate (@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'update'); }
+  @On(Events.GuildEmojiCreate) private onGuildEmojiCreate(@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'create'); }
+  @On(Events.GuildEmojiDelete) private onGuildEmojiDelete(@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'delete'); }
+  @On(Events.GuildEmojiUpdate) private onGuildEmojiUpdate(@IA() guildEmoji: GuildEmoji) { this.updateEmojis(guildEmoji, 'update'); }
 
   private updateEmojis(guildEmoji: GuildEmoji, action: 'create' | 'delete' | 'update'): any {
     if (!guildEmoji.name || !Object.keys(this.emojis).includes(guildEmoji.name.toLowerCase())) return;
-    this.emojis[guildEmoji.name as keyof typeof this.emojis] = action == 'delete' ? `{NO_EMOJI=:${guildEmoji.name}:}` : guildEmoji.toString()
+    this.emojis[guildEmoji.name as keyof typeof this.emojis] = action == 'delete' ? `{NO_EMOJI=:${guildEmoji.name}:}` : guildEmoji.toString();
   }
 }
