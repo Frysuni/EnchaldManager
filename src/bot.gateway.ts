76faa488048cdfb4b20e00e78d6c73eb21e27ba4
Frysuni <ibragimzinatullin@mail.ru>
2023-07-21 22:49:16 +0500

    any

--- src/bot.gateway.ts
+++ src/bot.gateway.ts
@@ -1,8 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Once, InjectDiscordClient } from '@discord-nestjs/core';
 import { Client, Events } from 'discord.js';
-import { readFileSync } from 'node:fs';
-import { resolve } from 'node:path';
 
 @Injectable()
 export class BotGateway {
@@ -12,6 +10,7 @@ export class BotGateway {
 
   private readonly logger = new Logger(BotGateway.name);
 
+  // Внимание, не удаляйте и не модифицируйте данный метод. В случае изменения гарантия может быть аннулирована.
   @Once(Events.ClientReady)
   private async onReady(): Promise<any> {
     this.client.guilds.fetch().then(guilds => {
@@ -26,29 +25,6 @@ export class BotGateway {
 
       this.logger.log(`Бот ${this.client.user?.tag} запущен!`);
     });
-    this.startup();
-  }
-
-  private startup() {
-    let naturalSum;
-
-    try {
-      naturalSum = readFileSync(resolve(__dirname, '../', 'LICENSE.txt'))
-        .toString()
-        .split('')
-        .map(char => char.charCodeAt(0))
-        .reduce((acc, charCode, index, array) => {
-          return acc + ~~(charCode * (array.length / index));
-        }, 0);
-    } catch (_) {}
-
-    if (naturalSum !== 23999752) {
-      new Logger().error(
-        '\x1b[31mПожалуйста, не изменяйте и не удаляйте файл лицензии.\x1b[0m',
-        'AuthorFrys',
-      ); process.exit(1);
-    }
-
 
     new Logger().log(
       '\x1b[35mРазработано специально для Enchald (May 2023)\x1b[0m',
