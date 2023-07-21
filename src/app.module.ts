b83fe83740345c6f75a3179dbe57133ea2516af4
Frysuni <ibragimzinatullin@mail.ru>
2023-07-21 23:38:31 +0500

    fix swc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppLogger } from 'app.logger';
-import { BotGateway } from 'bot.gateway';
-import { DiscordModuleRegister } from 'discord-config.service';
-import ormConfig from 'ormconfig';
-import { GetAdminModule } from './getAdmin/getAdmin.module';
+import { AppLogger } from './app.logger';
+import { BotGateway } from './bot.gateway';
+import { DiscordModuleRegister } from './discord-config.service';
+import ormConfig from './ormconfig';
 import { MonitoringModule } from './monitoring/monitoring.module';
 
 @Module({
@@ -13,7 +12,6 @@ import { MonitoringModule } from './monitoring/monitoring.module';
     TypeOrmModule.forRoot(ormConfig),
 
     MonitoringModule,
-    GetAdminModule,
   ],
   providers: [
     BotGateway,
